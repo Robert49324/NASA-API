8717dc8e90c42bac7a8de75e7d6c7e0ee18445dc
JustPC <robert49324@gmail.com>
2023-10-21 09:05:38 +0300

    Now worked in docker container

--- server/server.py
+++ server/server.py
@@ -1,16 +1,38 @@
 from flask import Flask, request, redirect
 import psycopg2
+import time
 
 app = Flask(__name__)
 
-conn = psycopg2.connect(database="images",
+conn = None
+
+while conn==None:
+    try:
+        conn = psycopg2.connect(database="images",
                         host="database",
                         user="postgres",
                         password="postgres",
                         port="5432")
+    except:
+        time.sleep(5)
     
 @app.route("/")
 def home():
+    with conn.cursor() as cursor:
+        cursor.execute('''SELECT EXISTS (
+                      SELECT 1
+                      FROM information_schema.tables
+                      WHERE table_schema = 'public' AND
+                            table_type = 'BASE TABLE' AND
+                            table_name = 'images'
+                    );''')
+        if not cursor.fetchone()[0]:
+            cursor.execute('''CREATE TABLE images (  
+                  id serial PRIMARY KEY,
+                  link VARCHAR(255),
+                  date DATE DEFAULT current_date
+                  );''')
+            conn.commit()
     date = request.args.get('date')
     with conn.cursor() as cursor:
         select ="SELECT link FROM images WHERE date=%s"
